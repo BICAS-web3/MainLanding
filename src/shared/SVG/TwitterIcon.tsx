9c314b548d34fec5908f0bd2ad5c7cc4867c7a77
Ilya3dev <ilya3dev@gmail.com>
2024-01-03 22:36:32 +0300

    fix

--- src/shared/SVG/TwitterIcon.tsx
+++ src/shared/SVG/TwitterIcon.tsx
@@ -5,17 +5,14 @@ import { IIconInterface } from "./types";
 export const TwitterIcon: FC<IIconInterface> = ({ className, onClick }) => {
   return (
     <svg
-      width="32"
-      height="32"
-      viewBox="0 0 32 32"
+      width="42"
+      height="42"
+      viewBox="0 0 42 42"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      onClick={onClick}
     >
-      <rect width="32" height="32" rx="16" fill="#121F24" />
       <path
-        d="M20.0258 9H22.1726L17.4825 14.9303L23 23H18.6799L15.2962 18.1057L11.4245 23H9.27646L14.2929 16.6569L9 9H13.4298L16.4883 13.4735L20.0258 9ZM19.2724 21.5785H20.4619L12.7834 10.3469H11.5069L19.2724 21.5785Z"
+        d="M32.7462 0.382812H39.0472L25.2813 17.789L41.4758 41.4748H28.7956L18.864 27.1095L7.50008 41.4748H1.19523L15.9192 22.857L0.383789 0.382812H13.3859L22.3631 13.5133L32.7462 0.382812ZM30.5347 37.3024H34.0262L11.4887 4.33607H7.74199L30.5347 37.3024Z"
         fill="#EAEAEA"
       />
     </svg>
